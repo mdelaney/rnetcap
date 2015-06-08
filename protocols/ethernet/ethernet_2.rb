8e9796a6a2fd44e0bd6582862d655349db96e295
mdelaney <matthew.l.delaney@gmail.com>
2015-06-07 21:23:51 -0600

    implemented ethernet2 frame frame_type setter and added tests

--- protocols/ethernet/ethernet_2.rb
+++ protocols/ethernet/ethernet_2.rb
@@ -10,7 +10,7 @@ module Protocol
       def initialize(destination, source, type, data, crc)
         @destination = MacAddress.new destination.unpack 'C6'
         @source = MacAddress.new source.unpack 'C6'
-        @type = type
+        self.frame_type = type
         @data = data
         @crc = crc
       end
@@ -23,6 +23,9 @@ module Protocol
       #     and ending before the last 4 bytes
       # 4 byte crc
       def self.decode(packet)
+        if packet.length < 18
+          raise ArgumentError, "EthernetII frame is too short"
+        end
         header = packet.unpack "a6 a6 n a#{packet.length - 18} N"
         # TODO: we should probably check the crc and throw an exception if its invalid
         Frame.new(*header)
@@ -34,16 +37,19 @@ module Protocol
       end
 
       # the type is a 16 bit uint
-      # this can be set by passing the 16 bit uint or a two byte array
-      def set_type(bytes)
-        #if bytes.is_a? Integer
-        #  if bytes < 0 or bytes > 0xFFFF
-        #    raise
+      # for this to be a valid ethernet 2 frame the value must be > 0x0600
+      def frame_type=(value)
+        if value <= 0x0600
+          raise RangeError, "the EthernetII type value must be greater than 0x0600"
+        elsif value > 0xFFFF
+          raise RangeError, "EthernetII type must be less than or equal to 0xFFFF"
+        end
+        @type = value
       end
 
       def to_s
-        type = @type.map {|i| i.to_s(16).rjust(2, '0').upcase}
-        crc = @crc.map {|i| i.to_s(16).rjust(2, '0').upcase}
+        type = @type.to_s(16).rjust(4, '0').upcase
+        crc = @crc.to_s(16).rjust(8, '0').upcase
 
         "destination: #{@destination}\n" +
         "source: #{@source}\n" +
--- spec/protocols/ethernet/frame_spec.rb
+++ spec/protocols/ethernet/frame_spec.rb
@@ -12,25 +12,42 @@ describe Protocol::Ethernet2::Frame do
                 "\xEE\x0E\xE3\xB8\x9B\xF0\xA1\x56" +
                 "\x80\x10\x10\x0E\x8D\x83\x00\x00" +
                 "\x01\x01\x08\x0A\x48\xF5\x5D\xCC").force_encoding 'ASCII-8BIT'
-  frame_destination_mac = "\x20\xE5\x2A\x11\xCA\x0C".force_encoding 'ASCII-8BIT'
-  frame_source_mac = "\xC3\xBC\xC8\xE7\xFA\x29".force_encoding 'ASCII-8BIT'
+  frame_dest_mac = "\x20\xE5\x2A\x11\xCA\x0C".force_encoding 'ASCII-8BIT'
+  frame_src_mac = "\xC3\xBC\xC8\xE7\xFA\x29".force_encoding 'ASCII-8BIT'
   frame_type = "\x08\x00".force_encoding 'ASCII-8BIT'
   frame_crc = "\x5F\x2F\x38\x92".force_encoding 'ASCII-8BIT'
-  frame = frame_destination_mac + frame_source_mac + frame_type + frame_data + frame_crc
 
-  frame_destination_string = "20:E5:2A:11:CA:0C"
-  frame_source_string = "C3:BC:C8:E7:FA:29"
+  frame_dest_string = "20:E5:2A:11:CA:0C"
+  frame_src_string = "C3:BC:C8:E7:FA:29"
   frame_type_integer = 2048
   frame_crc_integer = 1596930194
 
-  it "we can create a frame from valid data" do
-    frame = Frame.decode frame
+  it "can create a frame from valid data" do
+    raw_frame = frame_dest_mac + frame_src_mac + frame_type + frame_data + frame_crc
+    frame = Frame.decode raw_frame
     expect(frame).not_to be_nil
-    expect(frame.destination.to_s).to eq(frame_destination_string)
-    expect(frame.source.to_s).to eq(frame_source_string)
+    expect(frame.destination.to_s).to eq(frame_dest_string)
+    expect(frame.source.to_s).to eq(frame_src_string)
     expect(frame.type).to eq(frame_type_integer)
     expect(frame.crc).to eq(frame_crc_integer)
     expect(frame.data).to eq(frame_data)
+  end
+
+  it "throws an argument error if the frame size is less than 60 bytes" do
+    invalid_frame = frame_dest_mac + frame_src_mac + frame_type
+    expect {Frame.decode invalid_frame}.to raise_error(ArgumentError)
+  end
+
+  it "throws a range error if the type is less than 0x0600" do
+    invalid_frame_type = "\x00\x00".force_encoding 'ASCII-8BIT'
+    raw_frame = frame_dest_mac + frame_src_mac + invalid_frame_type + frame_data + frame_crc
+    expect {Frame.decode raw_frame}.to raise_error(RangeError)
+  end
+
+  it "throws a range error if the type is set greater than 0xFFFF" do
+    raw_frame = frame_dest_mac + frame_src_mac + frame_type + frame_data + frame_crc
+    frame = Frame.decode raw_frame
+    expect {frame.frame_type = 0x100000}.to raise_error(RangeError)
   end
 
 end
